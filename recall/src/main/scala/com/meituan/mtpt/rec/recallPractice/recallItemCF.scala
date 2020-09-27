68dd71957334a15222ebc5f6d986f64b6743cd47
江泽龙 <jiangzelong02@meituan.com>
2020-09-27 18:05:33 +0800

    fix

--- recall/src/main/scala/com/meituan/mtpt/rec/recallPractice/recallItemCF.scala
+++ recall/src/main/scala/com/meituan/mtpt/rec/recallPractice/recallItemCF.scala
@@ -107,13 +107,14 @@ object recallItemCF {
         val seqRecallDistinct = seqRecall.map(_._1).distinct   // 这里要给recall去重！
         val P = seqGT.size.toFloat
         val TP = seqGT.intersect(seqRecallDistinct).size.toFloat
-        val ref = seqRecallDistinct.size.toFloat
-        (TP, P, ref)
+        val recallSize = seqRecallDistinct.size.toFloat
+        (TP, P, recallSize)
     }.reduce((x, y) => (x._1 + y._1, x._2 + y._2, x._3 + y._3))
 
 //    println("debug 2" + recallHits2.toString)
     val rate = recallHits._1 / recallHits._2
     println(recallHits._1.toString + " " + recallHits._2.toString + " " + rate.toString)
+    println(recallHits._3)
   }
 
   def main(args: Array[String]): Unit ={
