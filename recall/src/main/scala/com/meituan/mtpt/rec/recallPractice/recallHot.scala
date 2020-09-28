97e91d80c72b0104ceec8103f1679cfc9fe24608
江泽龙 <jiangzelong02@meituan.com>
2020-09-28 14:33:32 +0800

    fix

--- recall/src/main/scala/com/meituan/mtpt/rec/recallPractice/recallHot.scala
+++ recall/src/main/scala/com/meituan/mtpt/rec/recallPractice/recallHot.scala
@@ -9,26 +9,26 @@ object recallHot {
   def main(args: Array[String]): Unit = {
 
     val priorData = env.hsc.sql("""
-                                  |select t_prior.order_id order_id,
-                                  |       t_prior.product_id product_id,
-                                  |       reordered,
-                                  |       order_dow,
-                                  |       order_hour_of_day
-                                  |  from ba_dealrank.recommend_star_order_products__prior t_prior
-                                  |  left join ba_dealrank.recommend_star_orders t_order
-                                  |    on t_order.order_id = t_prior.order_id
+    |select t_prior.order_id order_id,
+    |       t_prior.product_id product_id,
+    |       reordered,
+    |       order_dow,
+    |       order_hour_of_day
+    |  from ba_dealrank.recommend_star_order_products__prior t_prior
+    |  left join ba_dealrank.recommend_star_orders t_order
+    |    on t_order.order_id = t_prior.order_id
   """.stripMargin
     ).rdd
 
     val testData = env.hsc.sql("""
-                                 |select t_train.order_id order_id,
-                                 |       t_train.product_id product_id,
-                                 |       reordered,
-                                 |       order_dow,
-                                 |       order_hour_of_day
-                                 |  from ba_dealrank.recommend_star_order_products__train t_train
-                                 |  left join ba_dealrank.recommend_star_orders t_order
-                                 |    on t_order.order_id = t_train.order_id
+   |select t_train.order_id order_id,
+   |       t_train.product_id product_id,
+   |       reordered,
+   |       order_dow,
+   |       order_hour_of_day
+   |  from ba_dealrank.recommend_star_order_products__train t_train
+   |  left join ba_dealrank.recommend_star_orders t_order
+   |    on t_order.order_id = t_train.order_id
   """.stripMargin
     ).rdd
 
