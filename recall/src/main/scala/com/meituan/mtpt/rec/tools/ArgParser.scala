fab43e1fb98f5b568e4056f0b9bda312b9e1a8b6
江泽龙 <jiangzelong02@meituan.com>
2020-09-28 19:42:26 +0800

    add

--- recall/src/main/scala/com/meituan/mtpt/rec/tools/ArgParser.scala
+++ recall/src/main/scala/com/meituan/mtpt/rec/tools/ArgParser.scala
@@ -12,7 +12,7 @@ object ArgParser {
   def parseAsMap(args: Array[String]): Map[String, String] ={
     var argMap = Map[String, String]()
     for(row <- args){
-      val sp = row.split(":")
+      val sp = row.split("=")
       argMap += (sp(0) -> sp(1))
     }
     argMap
