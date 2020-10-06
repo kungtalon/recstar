1a038574f7ad0734205a6757e65bb789d3420b2e
江泽龙 <jiangzelong02@meituan.com>
2020-10-06 09:18:30 +0800

    add

---
+++ recall/src/main/scala/com/meituan/mtpt/rec/features.scala
@@ -0,0 +1,14 @@
+package com.meituan.mtpt.rec
+
+
+/**
+ * 计算所有的item特征和user特征
+ * 并存入到Hive表中
+ * itemFeature的索引为productId
+ * userFeature的索引为(userId, orderNum)，表示完成第orderNum个订单之后的user特征
+ */
+object features {
+
+
+
+}
